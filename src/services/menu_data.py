d5fcbc30b44d0bed05ce95deb14ac4e362a2bc00
Vitor Correa <vrcnode@gmail.com>
2024-05-24 16:46:12 -0300

    FEFE

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,12 @@
 # Req 3
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, mode="r") as file:
+            reader = csv.reader(file)
+            content = list(reader)
+            content.pop(0)
+            for name, price, ingredient, quantity in content:
+                print(name, price, ingredient, quantity)
